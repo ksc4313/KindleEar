57f3c5c7b87b1777a77401314ea1e8b022bc710a
ksc4313 <76030804+ksc4313@users.noreply.github.com>
2021-01-04 16:47:18 +0800

    Create greenhorn.py

---
+++ books/greenhorn.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from base import BaseFeedBook # 继承基类BaseFeedBook
+from lib.urlopener import URLOpener # 导入请求URL获取页面内容的模块
+from bs4 import BeautifulSoup # 导入BeautifulSoup处理模块
+
+# 返回此脚本定义的类名
+def getBook():
+    return greenhorn
+
+# 继承基类BaseFeedBook
+class greenhorn(BaseFeedBook):
+    # 设定生成电子书的元数据
+    title = u'greenhorn' # 设定标题
+    __author__ = u'greenhorn' # 设定作者
+    description = u'greenhorn' # 设定简介
+    language = 'zh-TW' # 设定语言
+    deliver_times=[2,10,18] #2:00,10:00,18:00
+    oldest_article=28800 #8*60*60
+    fulltext_by_readability = False
+    keep_only_tags = [
+        dict(name='div', class_='post hentry uncustomized-post-template'),
+    ]
+    remove_classes = ['post-labels']
+    # 指定要提取的包含文章列表的主题页面链接
+    # 每个主题是包含主题名和主题页面链接的元组
+    feeds = [
+        (u'綠角財經筆記', 'http://feeds2.feedburner.com/greenhornfinancefootnote?=format=xml'),
+    ]
