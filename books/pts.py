024e0fa8debf7fae98b25f7f969e0141c9bf14c9
ksc4313 <76030804+ksc4313@users.noreply.github.com>
2021-01-04 14:35:08 +0800

    Update pts.py

--- books/pts.py
+++ books/pts.py
@@ -23,7 +23,6 @@ class pts(BaseFeedBook):
         dict(name='div', class_='col-lg-8'),
         dict(name='article', class_='post-article'),
     ]
-    remove_classes = ['Google-special d-md-block','newsletter-subscribe','noteBar ga_trackEvent']
     # 指定要提取的包含文章列表的主题页面链接
     # 每个主题是包含主题名和主题页面链接的元组
     feeds = [
