ea3b0acc7ede8f24ed8a56a7a8f0372a47d9e9c0
ksc4313 <76030804+ksc4313@users.noreply.github.com>
2023-08-09 21:29:42 +0800

    Update bnext.py

--- books/bnext.py
+++ books/bnext.py
@@ -18,9 +18,44 @@ class bnext(BaseFeedBook):
     language = 'zh-TW' # 设定语言
     deliver_times=[2,10,18] #2:00,10:00,18:00
     oldest_article=28800 #8*60*60
-    fulltext_by_readability = True
+    fulltext_by_readability = False
     # 指定要提取的包含文章列表的主题页面链接
     # 每个主题是包含主题名和主题页面链接的元组
     feeds = [
-        (u'數位時代 BusinessNext', 'https://www.bnext.com.tw/rss'),
+        (u'數位時代 BusinessNext', 'https://www.bnext.com.tw/articles/p/1'),
     ]
+    # 设定内容页需要保留的标签
+    keep_only_tags = [
+        dict(name='div', class_='DynamicComp static htmlview'),
+        dict(name='div', class_='head-summary px-8 pt-4 pb-8'),
+        dict(name='h1', class_='head-title f24 font-weight-bold my-4'),
+    ]
+
+    # 提取每个主题页面下所有文章URL
+    def ParseFeedUrls(self):
+        urls = [] # 定义一个空的列表用来存放文章元组
+        # 循环处理fees中两个主题页面
+        for feed in self.feeds:
+            # 分别获取元组中主题的名称和链接
+            topic, url = feed[0], feed[1]
+            # 请求主题链接并获取相应内容
+            opener = URLOpener(self.host, timeout=self.timeout)
+            result = opener.open(url)
+            # 如果请求成功，并且页面内容不为空
+            if result.status_code == 200 and result.content:
+                # 将页面内容转换成BeatifulSoup对象
+                soup = BeautifulSoup(result.content, 'lxml')
+                # 找出当前页面文章列表中所有文章条目
+                items = soup.find_all(name='data-v-046cb521', class_='col-sm-12 col-md-4 col-lg-4 col-12 BG-Col')
+                # 循环处理每个文章条目
+                for item in items:
+                    title = item.a.string # 获取文章标题
+                    link = item.a.get('href') # 获取文章链接
+                    link = BaseFeedBook.urljoin(url, link) # 合成文章链接
+                    urls.append((topic, title, link, None)) # 把文章元组加入列表
+            # 如果请求失败通知到日志输出中
+            else:
+                self.log.warn('Fetch article failed(%s):%s' % \
+                    (URLOpener.CodeMap(result.status_code), url))
+        # 返回提取到的所有文章列表
+        return urls
